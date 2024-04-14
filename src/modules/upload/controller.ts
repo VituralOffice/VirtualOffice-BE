a0eee42524ba603cfab92cc1ce3fa16e7a311044
Duc Hoang Ho <duchoang206h@gmail.com>
2024-04-14 19:12:41 +0000

    feat: add file validator

--- src/modules/upload/controller.ts
+++ src/modules/upload/controller.ts
@@ -1,14 +1,43 @@
-import { Controller, HttpCode, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, HttpCode, ParseFilePipeBuilder, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { IUploadService } from './adapter';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 
 @Controller('upload')
+@ApiTags("upload")
 export class UploadController {
   constructor(private readonly uploadService: IUploadService) {}
-  @Post()
   @HttpCode(200)
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @Post()
   @UseInterceptors(FileInterceptor('file'))
-  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+  async uploadFile(
+    @UploadedFile(
+    new ParseFilePipeBuilder()
+    .addFileTypeValidator({
+      fileType: 'jpeg',
+    })
+    .addFileTypeValidator({
+      fileType: 'png',
+    })
+    .addMaxSizeValidator({
+      maxSize: 50*1024
+    })
+    .build({
+      errorHttpStatusCode: 422
+    }),
+  ) file: Express.Multer.File) {
     const result = await this.uploadService.upload(file.buffer, file.originalname);
     return {
       result,
