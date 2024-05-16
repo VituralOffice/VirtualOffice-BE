a0f60a94ed975741daaccea0d447901f9431bafd
Đức Hoàng <duchoang206h@gmail.com>
2024-05-16 23:36:37 +0700

    temp: chat

--- src/modules/rooms/commands/ChatMessageUpdateCommand.ts
+++ src/modules/rooms/commands/ChatMessageUpdateCommand.ts
@@ -1,30 +1,35 @@
 import { Command } from '@colyseus/command';
 import { Client } from 'colyseus';
 import { IOfficeState } from '../../../types/IOfficeState';
-import { ChatMessage } from '../schema/OfficeState';
+import { ChatMessage, MapMessage } from '../schema/OfficeState';
 
 type Payload = {
   client: Client;
   content: string;
+  chatId: string;
 };
 
 export default class ChatMessageUpdateCommand extends Command<IOfficeState, Payload> {
   execute(data: Payload) {
-    const { client, content } = data;
+    const { client, content, chatId } = data;
     const player = this.room.state.players.get(client.sessionId);
-    const chatMessages = this.room.state.chatMessages;
-
-    if (!chatMessages) return;
-
-    /**
-     * Only allow server to store a maximum of 100 chat messages:
-     * remove the first element before pushing a new one when array length is >= 100
-     */
-    if (chatMessages.length >= 100) chatMessages.shift();
-
+    const mapMessages = this.room.state.mapMessages;
+    console.log({ mapMessages });
     const newMessage = new ChatMessage();
     newMessage.author = player.fullname;
     newMessage.content = content;
-    chatMessages.push(newMessage);
+    if (!mapMessages.has(chatId)) {
+      const mapMessage = new MapMessage();
+      mapMessage.id = chatId;
+      mapMessage.messages = [newMessage];
+      mapMessages[chatId] = mapMessage;
+    } else {
+      /**
+       * Only allow server to store a maximum of 100 chat messages:
+       * remove the first element before pushing a new one when array length is >= 100
+       */
+      if (mapMessages[chatId].messages.length >= 100) mapMessages[chatId].messages.shift();
+      mapMessages[chatId].messages.push(newMessage);
+    }
   }
 }
