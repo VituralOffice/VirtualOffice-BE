d3d2bca53012cce43cc4ea15bd046ceb3dc4c357
tune <tunguyenhv@gmail.com>
2024-05-30 00:07:51 +0700

    +++

--- src/modules/rooms/commands/MeetingUpdateArrayCommand.ts
+++ src/modules/rooms/commands/MeetingUpdateArrayCommand.ts
@@ -2,6 +2,11 @@ import { Command } from '@colyseus/command';
 import { Client } from 'colyseus';
 import { IOfficeState } from '../../../types/IOfficeState';
 
+type Payload = {
+  client: Client;
+  meetingId: string;
+}
+
 type AddMemberPayload = {
   client: Client;
   meetingId: string;
