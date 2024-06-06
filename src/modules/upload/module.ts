271973045f0d481c0b5c0c1b2c364aec6e74e7f4
Đức Hoàng <duchoang206h@gmail.com>
2024-06-06 14:21:46 +0700

    fix: multer

--- src/modules/upload/module.ts
+++ src/modules/upload/module.ts
@@ -3,8 +3,15 @@ import { UploadController } from './controller';
 import { UploadService } from './service';
 import { SecretsModule } from '../global/secrets/module';
 import { AWSModule } from '../aws/module';
+import { MulterModule } from '@nestjs/platform-express';
 @Module({
-  imports: [SecretsModule, AWSModule],
+  imports: [
+    SecretsModule,
+    AWSModule,
+    MulterModule.register({
+      limits: { fileSize: 100 * 1024 * 1024 },
+    }),
+  ],
   controllers: [UploadController],
   providers: [UploadService],
   exports: [UploadService],
